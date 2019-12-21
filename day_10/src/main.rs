1c22c81e042f20546f016c10af088aeab17bbada
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2019-12-21 23:37:36 +0100

    Add day10-2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,5 +1,7 @@
 use num_rational::Rational64;
+use std::cmp::{Ord, Ordering};
 use std::collections::HashMap;
+use std::f64;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::rc::{Rc, Weak};
@@ -14,8 +16,8 @@ type WrappedAsteroid = Rc<Asteroid>;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum CrudeAngle {
-    Above(Rational64),
-    Below(Rational64),
+    Right(Rational64),
+    Left(Rational64),
     VerticallyAbove,
     VerticallyBelow,
 }
@@ -39,10 +41,62 @@ impl CrudeAngle {
         match (dx, dy) {
             (0, dy) if dy >= 0 => VerticallyAbove,
             (0, dy) if dy < 0 => VerticallyBelow,
-            (dx, dy) if dy >= 0 => Above(Rational64::new(dy, dx)),
-            _ => Below(Rational64::new(dy, dx)),
+            (dx, dy) if dx > 0 => Right(Rational64::new(dy, dx)),
+            _ => Left(Rational64::new(dy, dx)),
         }
     }
+
+    /// Compute the angle to the 12 o'clock position (clockwise) in range [0, 2*pi)
+    fn to_rad_12_oclock(&self) -> f64 {
+        use CrudeAngle::*;
+        match self {
+            VerticallyAbove => 0.0,
+            VerticallyBelow => f64::consts::PI,
+            Right(r) => f64::consts::FRAC_PI_2 - ratio_to_f64(r).atan(),
+            Left(r) => f64::consts::FRAC_PI_2 * 3.0 - ratio_to_f64(r).atan(),
+        }
+    }
+}
+
+impl Ord for CrudeAngle {
+    /// The order is from small to large:
+    /// 1. VerticallyAbove (0 degrees)
+    /// 2. Right(compare angles)
+    /// 3. VerticallyAbove (180 degrees)
+    /// 4. Left(compare angles)
+    ///
+    /// Since atan is monotonic we do not need to call it.
+    fn cmp(&self, other: &Self) -> Ordering {
+        use CrudeAngle::*;
+        use Ordering::*;
+        if self == other {
+            Equal
+        } else {
+            match (self, other) {
+                (Right(s), Right(o)) => (-s).cmp(&(-o)),
+                (Left(s), Left(o)) => (-s).cmp(&(-o)),
+                // equal case is handled above
+                (VerticallyAbove, _) => Less,
+                (_, VerticallyAbove) => Greater,
+                // all cases with values smaller than Right(_) are handled
+                (Right(_), _) => Less,
+                (_, Right(_)) => Greater,
+                // all cases with values smaller than VerticallyBelow(_) are handled
+                (VerticallyBelow, _) => Less,
+                (_, VerticallyBelow) => Greater,
+            }
+        }
+    }
+}
+
+impl PartialOrd for CrudeAngle {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn ratio_to_f64(r: &Rational64) -> f64 {
+    *r.numer() as f64 / *r.denom() as f64
 }
 
 /// VisibleSet encapsulates the set of astroids that are currently visible
@@ -78,8 +132,12 @@ impl VisibleSet {
         }
     }
 
-    fn get_visible(&self) -> Vec<Asteroid> {
-        self.angle_to_ast.values().map(|a| *a).collect()
+    fn get_visible(&self) -> impl Iterator<Item = &Asteroid> {
+        self.angle_to_ast.values()
+    }
+
+    fn get_angle_visible(&self) -> impl Iterator<Item = (&CrudeAngle, &Asteroid)> {
+        self.angle_to_ast.iter()
     }
 }
 
@@ -95,15 +153,15 @@ impl Asteroid {
     }
 
     fn sqdist_to(&self, other: &Self) -> i64 {
-        let dx = self.pos.x - other.pos.x;
-        let dy = self.pos.y - other.pos.y;
+        let dx = other.pos.x - self.pos.x;
+        let dy = other.pos.y - self.pos.y;
 
         dx * dx + dy * dy
     }
 
     fn angle_to(&self, other: &Self) -> CrudeAngle {
-        let dx = self.pos.x - other.pos.x;
-        let dy = self.pos.y - other.pos.y;
+        let dx = other.pos.x - self.pos.x;
+        let dy = self.pos.y - other.pos.y; // NOTE: y gets positive when going to the bottom!
 
         CrudeAngle::new(dx, dy)
     }
@@ -127,11 +185,15 @@ impl System {
             }
         }
 
-        Self::new(asteroids, width, height)
+        Self::new(&asteroids, width, height)
     }
 
-    pub fn new(asteroids: Vec<Asteroid>, width: usize, height: usize) -> System {
+    pub fn new(starting: &[Asteroid], width: usize, height: usize) -> System {
         let mut pos_to_ast = HashMap::new();
+        let mut asteroids = Vec::new();
+        asteroids.resize(starting.len(), Asteroid::new(-1, -1));
+
+        asteroids.copy_from_slice(starting);
 
         for a in asteroids.iter() {
             pos_to_ast.insert(a.pos, *a);
@@ -145,21 +207,52 @@ impl System {
         }
     }
 
-    pub fn get(&self, x: i64, y: i64) -> Option<Asteroid> {
+    pub fn get(&self, x: i64, y: i64) -> Option<&Asteroid> {
         match self.pos_to_ast.get(&Position { x, y }) {
             None => None,
-            Some(ast) => Some(*ast),
+            Some(ast) => Some(ast),
         }
     }
 
     pub fn print(&self) {
+        self.print_(None);
+    }
+
+    pub fn print_with_root(self, root: &Asteroid) {
+        self.print_(Some(root));
+    }
+
+    fn print_(&self, root: Option<&Asteroid>) {
         for y in 0..self.height {
             for x in 0..self.width {
+                if let Some(Asteroid {
+                    pos: Position { x: rx, y: ry },
+                }) = root
+                {
+                    if x as i64 == *rx && y as i64 == *ry {
+                        print!("X");
+                        continue;
+                    }
+                }
                 print!(
                     "{}",
                     match self.get(x as i64, y as i64) {
-                        None => '.',
-                        Some(_) => '#',
+                        None => String::from("."),
+                        Some(_) => {
+                            if self.asteroids.len() <= 10 {
+                                let mut num: Option<String> = None;
+                                for (i, ast) in self.asteroids.iter().enumerate() {
+                                    let (x, y) = (x as i64, y as i64);
+                                    let pos = Position { x, y };
+                                    if ast.pos == pos {
+                                        num = Some(i.to_string());
+                                    }
+                                }
+                                num.unwrap()
+                            } else {
+                                String::from("#")
+                            }
+                        }
                     }
                 );
             }
@@ -173,8 +266,20 @@ impl System {
         for ast in self.asteroids.iter() {
             checker.check(ast);
         }
+        checker.get_visible().map(|a| *a).collect()
+    }
 
-        checker.get_visible()
+    fn visible_from_sorted(&self, root: Asteroid) -> Vec<Asteroid> {
+        let mut checker = VisibleSet::new(root);
+
+        for ast in self.asteroids.iter() {
+            checker.check(ast);
+        }
+        let mut with_angles: Vec<_> = checker.get_angle_visible().map(|(c, a)| (*c, *a)).collect();
+
+        with_angles.sort_by_key(|t| t.0);
+
+        with_angles.iter().map(|t| t.1).collect()
     }
 
     fn visible_from_all(&self) -> HashMap<Asteroid, usize> {
@@ -182,6 +287,23 @@ impl System {
             .iter()
             .map(|a| (*a, self.visible_from(*a).len()))
             .collect()
+    }
+
+    fn remove_asteroids(&mut self, to_remove: &[Asteroid]) {
+        for ast in to_remove.iter() {
+            if self.pos_to_ast.contains_key(&ast.pos) && self.asteroids.contains(ast) {
+                self.pos_to_ast.remove(&ast.pos);
+                // very inefficient -> do not care
+                for (i, contained) in self.asteroids.iter().enumerate() {
+                    if contained == ast {
+                        self.asteroids.remove(i);
+                        break;
+                    }
+                }
+            } else {
+                panic!("Did not find asteroid to remove!");
+            }
+        }
     }
 }
 
@@ -197,6 +319,18 @@ fn max_key<K, V: PartialOrd, I: Iterator<Item = (K, V)>>(iter: &mut I) -> (K, V)
     (arg, max)
 }
 
+fn banner(system: &System) {
+    println!();
+    println!("{}", str::repeat("=", system.width));
+    println!();
+}
+
+fn clear_screen()
+{
+    // print!("{}[2J", 27 as char);
+    print!("\x1B[2J");
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
 
@@ -207,27 +341,72 @@ fn main() {
     let file = File::open(&args[0]).unwrap();
     let reader = BufReader::new(&file);
 
-    let system = System::parse(reader.lines().map(|l| l.unwrap()));
+    let mut system = System::parse(reader.lines().map(|l| l.unwrap()));
     // eprintln!("{:?}", system);
     system.print();
 
     let visible_from_all = system.visible_from_all();
 
     let (location, num_visible) = max_key(&mut visible_from_all.iter());
-    println!();
-    println!("{}", str::repeat("=", system.width));
-    println!();
+
+    banner(&system);
+
     println!(
         "Best location: {:?} #Visible: {}",
-        location.pos, num_visible+1 // count yourself
+        location.pos,
+        num_visible
     );
     println!();
 
     let mut visible = system.visible_from(*location);
-
-    visible.push(*location);
-
     // eprintln!("Visible from {:?}: {:?}", location.pos, visible);
-    let subset = System::new(visible, system.width, system.height);
-    subset.print();
+    let subset = System::new(&visible, system.width, system.height);
+    subset.print_with_root(location);
+
+    banner(&system);
+
+    let to_remove = 200;
+    let mut num_removed = 0;
+
+    loop {
+        visible = system.visible_from_sorted(*location);
+
+        // let visible = &visible[..10];
+
+        let mut last_rad = 0.0;
+
+        clear_screen();
+
+        for (i, ast) in visible.iter().enumerate() {
+            let current_rad = location.angle_to(ast).to_rad_12_oclock();
+            assert!(current_rad >= last_rad);
+            last_rad = current_rad;
+            println!(
+                "Vaporizing #{}: {:?} (angle: {})",
+                num_removed + i + 1,
+                ast,
+                current_rad
+            );
+        }
+        System::new(&visible, system.width, system.height).print_with_root(location);
+        std::thread::sleep(std::time::Duration::from_millis(100));
+
+        if visible.len() + num_removed >= to_remove {
+            break;
+        }
+
+        system.remove_asteroids(&visible);
+        num_removed += visible.len();
+    }
+
+    banner(&system);
+
+    let target = visible[to_remove - num_removed - 1];
+
+    println!(
+        "The {}th asteroid to vaporize is {:?} (answer: {})..",
+        to_remove,
+        target,
+        target.pos.x * 100 + target.pos.y
+    );
 }
