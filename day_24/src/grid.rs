59fa29188590a69ed4841ecf1ceed9b34ef74f66
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-01-05 10:47:56 +0100

    Add day24-1

---
+++ day_24/src/grid.rs
@@ -0,0 +1,232 @@
+use std::cmp::{max, min};
+use std::collections::HashMap;
+use std::fmt;
+use std::io::prelude::*;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub struct Position {
+    pub x: i64,
+    pub y: i64,
+}
+
+#[derive(Debug,Clone)]
+pub struct Grid<T> {
+    grid: HashMap<Position, T>,
+}
+
+#[derive(Debug)]
+pub struct Dimensions {
+    pub x_min: i64,
+    pub x_max: i64,
+    pub y_min: i64,
+    pub y_max: i64,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum Direction {
+    North,
+    South,
+    West,
+    East,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum Turn {
+    Left,
+    Right,
+}
+
+impl Dimensions {
+    pub fn width(&self) -> i64 {
+        self.x_max - self.x_min
+    }
+
+    pub fn height(&self) -> i64 {
+        self.y_max - self.y_min
+    }
+}
+
+impl Turn {
+    pub fn all() -> &'static [Self] {
+        use Turn::*;
+        static VARIANTS: &'static [Turn] = &[Left, Right];
+        VARIANTS
+    }
+}
+
+impl Into<String> for Turn {
+    fn into(self) -> String {
+        use Turn::*;
+        match self {
+            Right => String::from("R"),
+            Left => String::from("L"),
+        }
+    }
+}
+
+impl Position {
+    pub fn step(&self, dir: &Direction) -> Self {
+        use Direction::*;
+        let Position { x, y } = self;
+        let (dx, dy) = match *dir {
+            North => (0, -1),
+            South => (0, 1),
+            West => (-1, 0),
+            East => (1, 0),
+        };
+        Position {
+            x: x + dx,
+            y: y + dy,
+        }
+    }
+
+    /// manhatten distance
+    pub fn mh(&self, other: &Self) -> usize {
+        ((self.x - other.x).abs() + (self.y - other.y).abs()) as usize
+    }
+}
+
+impl Direction {
+    pub fn all() -> &'static [Direction] {
+        use Direction::*;
+        static VARIANTS: &'static [Direction] = &[North, South, West, East];
+        VARIANTS
+    }
+
+    pub fn invert(&self) -> Self {
+        use Direction::*;
+        match self {
+            North => South,
+            South => North,
+            West => East,
+            East => West,
+        }
+    }
+
+    pub fn to_turn(&self, other: &Self) -> Turn {
+        use Direction::*;
+        use Turn::*;
+        match (self, other) {
+            (North, West) => Right,
+            (North, East) => Left,
+            (South, East) => Right,
+            (South, West) => Left,
+            (West, North) => Right,
+            (West, South) => Left,
+            (East, South) => Right,
+            (East, North) => Left,
+            (_, _) => panic!("Unsupported turn!"),
+        }
+    }
+
+    pub fn turn(&self, turn: &Turn) -> Direction {
+        use Direction::*;
+        use Turn::*;
+        match (self, turn) {
+            (North, Right) => East,
+            (North, Left) => West,
+            (South, Right) => West,
+            (South, Left) => East,
+            (West, Right) => North,
+            (West, Left) => South,
+            (East, Right) => South,
+            (East, Left) => North,
+        }
+    }
+}
+
+impl<T> Grid<T>
+where
+    T: Default,
+    T: fmt::Display,
+    T: Copy,
+{
+    pub fn new() -> Grid<T> {
+        Grid {
+            grid: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, pos: &Position) -> T {
+        match self.grid.get(pos) {
+            None => Default::default(),
+            Some(elem) => *elem,
+        }
+    }
+
+    pub fn get_existing(&self, pos: &Position) -> Option<T> {
+        self.grid.get(pos).map(|e| *e)
+    }
+
+    pub fn add(&mut self, pos: Position, tile: T) {
+        self.grid.insert(pos, tile);
+    }
+
+    pub fn get_dims(&self) -> Dimensions {
+        let mut x_min = std::i64::MAX;
+        let mut y_min = std::i64::MAX;
+        let mut x_max = -std::i64::MAX;
+        let mut y_max = -std::i64::MAX;
+
+        for Position { x, y } in self.grid.keys() {
+            x_min = min(x_min, *x);
+            y_min = min(y_min, *y);
+            x_max = max(x_max, *x);
+            y_max = max(y_max, *y);
+        }
+
+        Dimensions {
+            x_min,
+            x_max,
+            y_min,
+            y_max,
+        }
+    }
+
+    pub fn print(&self) {
+        self.print_custom(|_: &Position| -> Option<T> { None });
+    }
+
+    pub fn print_custom<F, I>(&self, f_override: F)
+    where
+        F: Fn(&Position) -> Option<I>,
+        I: fmt::Display,
+    {
+        print!("{}", self.write_custom(f_override));
+    }
+
+    pub fn write(&self) -> String {
+        self.write_custom(|_: &Position| -> Option<T> { None })
+    }
+
+    pub fn write_custom<F, I>(&self, f_override: F) -> String
+    where
+        F: Fn(&Position) -> Option<I>,
+        I: fmt::Display,
+    {
+        let mut output: Vec<u8> = Vec::new();
+        let dims = self.get_dims();
+
+        for y in dims.y_min..dims.y_max + 1 {
+            for x in dims.x_min..dims.x_max + 1 {
+                let pos = Position { x, y };
+                let to_print = match f_override(&pos) {
+                    None => self.get(&pos).to_string(),
+                    Some(special) => special.to_string(),
+                };
+                write!(output, "{}", to_print);
+            }
+            writeln!(output);
+        }
+
+        String::from_utf8(output).expect("Error formatting grid!")
+    }
+
+    pub fn iter(&self) -> std::collections::hash_map::Iter<Position, T> {
+        self.grid.iter()
+    }
+
+    pub fn values(&self) -> std::collections::hash_map::Values<Position, T> {
+        self.grid.values()
+    }
+}
--- day_24/src/main.rs
+++ day_24/src/main.rs
@@ -1,3 +1,175 @@
+use std::cmp::min;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::convert::From;
+use std::default::Default;
+use std::fmt;
+use std::fs::read_to_string;
+
+mod grid;
+
+use grid::{Dimensions, Direction, Grid, Position};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum Tile {
+    Empty,
+    Bugs,
+}
+
+#[derive(Debug)]
+struct GameOfEris {
+    grid: Grid<Tile>,
+    dims: Dimensions,
+}
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        use Tile::*;
+        match c {
+            '.' => Empty,
+            '#' => Bugs,
+            _ => panic!("Invalid input for tile."),
+        }
+    }
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        Tile::Empty
+    }
+}
+
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Tile::*;
+        let to_write = match self {
+            Empty => '.',
+            Bugs => '#',
+        };
+        f.write_str(&to_write.to_string())
+    }
+}
+
+impl GameOfEris {
+    pub fn new(filename: &str) -> GameOfEris {
+        let raw = read_to_string(filename).expect("Could not read input file.");
+        let mut grid = Grid::new();
+
+        for (y, line) in raw.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                let tile = Tile::from(c);
+                let pos = Position {
+                    x: x as i64,
+                    y: y as i64,
+                };
+                grid.add(pos, tile);
+            }
+        }
+        grid.print();
+        let dims = grid.get_dims();
+        assert_eq!(dims.x_min, 0);
+        assert_eq!(dims.y_min, 0);
+        GameOfEris { grid, dims }
+    }
+
+    pub fn step(&mut self) {
+        let mut next = self.grid.clone();
+
+        for (pos, prev) in self.grid.iter() {
+            let mut num_bugs_neighbouring = 0;
+            for dir in Direction::all() {
+                num_bugs_neighbouring += match self.grid.get(&pos.step(dir)) {
+                    Tile::Empty => 0,
+                    Tile::Bugs => 1,
+                };
+            }
+
+            next.add(
+                *pos,
+                match (prev, num_bugs_neighbouring) {
+                    (Tile::Bugs, 1) => Tile::Bugs,
+                    (Tile::Bugs, _) => Tile::Empty,
+                    (Tile::Empty, 1) | (Tile::Empty, 2) => Tile::Bugs,
+                    _ => *prev,
+                },
+            );
+        }
+        self.grid = next;
+    }
+
+    pub fn biodiversity(&self) -> u64 {
+        let mut res = 0;
+        let len_y = self.dims.y_max - self.dims.y_min + 1;
+        for (pos, t) in self.grid.iter() {
+            if let Tile::Bugs = t {
+                res += 2u64.pow((pos.x + len_y * pos.y) as u32);
+            }
+        }
+        res
+    }
+
+    pub fn write(&self) -> String {
+        self.grid.write()
+    }
+
+    pub fn print(&self) {
+        self.grid.print();
+    }
+
+    pub fn find_first_repeating(&mut self) {
+        let mut seen: HashSet<String> = HashSet::new();
+        seen.insert(self.write());
+
+        loop {
+            self.step();
+            let state = self.write();
+
+            println!("\n{}", state);
+
+            std::thread::sleep(std::time::Duration::from_millis(25));
+
+            if seen.contains(&state) {
+                break;
+            } else {
+                seen.insert(state);
+            }
+        }
+    }
+}
+
+fn clear_screen() {
+    // print!("{}[2J", 27 as char);
+    print!("\x1B[2J");
+}
+
+fn backspace() {
+    print!("\x08");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut eris = GameOfEris::new(
+        &std::env::args()
+            .skip(1)
+            .next()
+            .expect("Filename not provided."),
+    );
+    eris.find_first_repeating();
+    println!();
+    eris.print();
+    println!();
+    println!("{}", eris.biodiversity());
+    println!();
+
+    eprintln!("{:?}", eris.dims);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_biodiveristy()
+    {
+        let eris = GameOfEris::new("example_01.txt");
+        assert_eq!(eris.biodiversity(), 2129920);
+    }
 }
